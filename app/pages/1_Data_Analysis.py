a050970e8f61ac40fc90926ce3dcf3cd7dd325df
Abhishek Vaidyanathan <abhishekvaidyanathan@Abhisheks-MacBook-Pro-4.local>
2022-11-16 17:54:20 +0800

    planning area changes

--- app/pages/1_Data_Analysis.py
+++ app/pages/1_Data_Analysis.py
@@ -51,109 +51,187 @@ marker_data =(
     }
 )
 
-f = open('../data/new_bus_transport_graph_new_dist.json')
-data = json.load(f)
-marker_data = data['nodes']
-
-def icon_create_function(feat):
-    feature = """
-        function(cluster) {
-            var markers = cluster.getAllChildMarkers();
-            var sum = 0;
-            for (var i = 0; i < markers.length; i++) {
-                sum += markers[i].options.props.
-                """ + feat + """;
-            }
-            var avg = sum/cluster.getChildCount();
-            avg = avg.toFixed(2);
-
-            return L.divIcon({
-                html: '<b>' + avg + '</b>',
-                className: 'marker-cluster marker-cluster-small',
-                iconSize: new L.Point(20, 20)
-            });
-        }
-    """
-    return feature
-
-feature = 'deficit'
-st.write("feature = " + feature)
-marker_cluster = MarkerCluster(icon_create_function=icon_create_function(feature))
-
-for marker_item in marker_data:
-    marker = MarkerWithProps(
-        location=[marker_item['latitude'],marker_item['longitude']],
-        props = {feature: marker_item[feature]}
-    )
-    marker.add_to(marker_cluster)
-
-marker_cluster.add_to(map)    
-
-st_folium(map, width=1200, height=600)
-
-
-feature = 'pop_estimate'
-st.write("feature = " + feature)
-
-new_feat = []
-for i in marker_data:
-    new_feat.append([i['latitude'],i['longitude'],i[feature]])
-new_feat = pd.DataFrame(new_feat)
-heat_data = new_feat.values.tolist()
-
-m=folium.Map([1.36, 103.83],zoom_start=11.5,tiles="cartodbpositron")
-hm = HeatMap(heat_data,gradient={0.1: 'blue', 0.3: 'lime', 0.5: 'yellow', 0.7: 'orange', 1: 'red'}, 
-                min_opacity=0.05, 
-                max_opacity=0.9, 
-                radius=25,
-                use_local_extrema=False).add_to(m)
-
-st_folium(m, width=1200, height=600)
-
-period_selection = st.sidebar.radio('Period:', ['AM_Offpeak_Freq', 'AM_Peak_Freq', 'PM_Offpeak_Freq', 'PM_Peak_Freq'])
-
-line_data = data['links']
-
-f = open('../data/bus_stop_full_info.json')
-bus_stops = json.load(f)
+# def read_json(file_path):
+# f = open('../data/new_bus_transport_graph_new_dist.json')
+# data = json.load(f)
+@st.cache(allow_output_mutation=True) 
+def read_json(file_path):
+    f = open(file_path)
+    data = json.load(f)
+    f.close()
+    return data
 
 @st.cache(allow_output_mutation=True) 
-def process_lat_longs(line_data,bus_stops):
-    pos = []
-    period = []
-    for i in line_data:
+def load_all_data():
+    transport_graph = read_json("../data/new_bus_transport_graph_new_dist.json")
+    bus_stop = read_json('../data/bus_stop_full_info.json')
+
+    return transport_graph,bus_stop
+
+data,bus_stops = load_all_data()
+
+
+# data = read_json("../data/new_bus_transport_graph_new_dist.json")
+
+# f = open('../data/bus_stop_full_info.json')
+# bus_stops = json.load(f)
+marker_data = data['nodes']
+
+# def icon_create_function(feat):
+#     feature = """
+#         function(cluster) {
+#             var markers = cluster.getAllChildMarkers();
+#             var sum = 0;
+#             for (var i = 0; i < markers.length; i++) {
+#                 sum += markers[i].options.props.
+#                 """ + feat + """;
+#             }
+#             var avg = sum/cluster.getChildCount();
+#             avg = avg.toFixed(2);
+
+#             return L.divIcon({
+#                 html: '<b>' + avg + '</b>',
+#                 className: 'marker-cluster marker-cluster-small',
+#                 iconSize: new L.Point(20, 20)
+#             });
+#         }
+#     """
+#     return feature
+
+# feature = 'deficit'
+# st.write("feature = " + feature)
+# marker_cluster = MarkerCluster(icon_create_function=icon_create_function(feature))
+
+# for marker_item in marker_data:
+#     marker = MarkerWithProps(
+#         location=[marker_item['latitude'],marker_item['longitude']],
+#         props = {feature: marker_item[feature]}
+#     )
+#     marker.add_to(marker_cluster)
+
+# marker_cluster.add_to(map)    
+
+# st_folium(map, width=1200, height=600)
+
+
+# feature = 'pop_estimate'
+# st.write("feature = " + feature)
+
+# new_feat = []
+# for i in marker_data:
+#     new_feat.append([i['latitude'],i['longitude'],i[feature]])
+# new_feat = pd.DataFrame(new_feat)
+# heat_data = new_feat.values.tolist()
+
+# m=folium.Map([1.36, 103.83],zoom_start=11.5,tiles="cartodbpositron")
+# hm = HeatMap(heat_data,gradient={0.1: 'blue', 0.3: 'lime', 0.5: 'yellow', 0.7: 'orange', 1: 'red'}, 
+#                 min_opacity=0.05, 
+#                 max_opacity=0.9, 
+#                 radius=25,
+#                 use_local_extrema=False).add_to(m)
+
+# st_folium(m, width=1200, height=600)
+
+# period_selection = st.sidebar.radio('Period:', ['AM_Offpeak_Freq', 'AM_Peak_Freq', 'PM_Offpeak_Freq', 'PM_Peak_Freq'])
+
+# line_data = data['links']
+
+# def process_lat_longs(line_data,bus_stops):
+#     pos = []
+#     period = []
+#     for i in line_data:
+#         try:
+#             source = bus_stops[i['source']]
+#             target = bus_stops[i['target']]
+#             pos.append(((source['Latitude'], source['Longitude']),(target['Latitude'], target['Longitude'])))
+#             # print(pos[-1])
+#             period.append(i[period_selection])
+#         except:
+#             pass
+
+#     return pos,period
+
+# pos,period=process_lat_longs(line_data,bus_stops)
+
+
+# values = st.sidebar.slider(
+#      'Select frequency range:',
+#      floor(min(period)), ceil(np.percentile(period, 95)), (floor(min(period)), ceil(np.percentile(period, 95))))
+
+
+# inperiod = [True if (i >= values[0] and i <= values[1]) else False for i in period]
+# color_dict = {True:'#17becf', False:'#D3D3D3'}
+
+# base=folium.Map([1.36, 103.83],zoom_start=11.5,tiles="cartodbpositron")
+# for i in range(len(pos)):
+#     # for pos_lat_long in bus_stops:
+#     #     # print("printing:",pos_lat_long)
+#     folium.PolyLine(
+#             pos[i], # tuple of coordinates 
+#             color = color_dict[inperiod[i]], # map each segment with the speed 
+#             # colormap = color_dict, # map each value with a color 
+#             ).add_to(base)
+#         # print(pos_lat_long)
+# st_folium(base, width=1200, height=600)
+
+@st.cache(allow_output_mutation=True) 
+def filter_by_planning_area(transport_graph,planningAreaFilter,bus_info):
+    nodes_data = transport_graph['nodes']
+    filter_nodes = []
+    filter_links = []
+
+    for i in nodes_data:
+        if(i['planningArea'] == planningAreaFilter):
+            filter_nodes.append(i['id'])
+    links_data = transport_graph['links']
+    for link in links_data:
         try:
-            source = bus_stops[i['source']]
-            target = bus_stops[i['target']]
-            pos.append(((source['Latitude'], source['Longitude']),(target['Latitude'], target['Longitude'])))
-            # print(pos[-1])
-            period.append(i[period_selection])
+            if(link['source'] in filter_nodes or link['target'] in filter_nodes):
+                filter_links.append(((bus_info[link['source']]['Latitude'],bus_info[link['source']]['Longitude']),(bus_info[link['target']]['Latitude'],bus_info[link['target']]['Longitude'])))
         except:
             pass
+    
+    return filter_links
 
-    return pos,period
+@st.cache(allow_output_mutation=True) 
+def get_planning_areas(transport_graph):
+    planning_areas = []
+    for i in transport_graph['nodes']:
+        planning_areas.append(i['planningArea'])
+    return list(set(planning_areas))
 
-pos,period=process_lat_longs(line_data,bus_stops)
+planning_areas = get_planning_areas(data)
 
+st.session_state.disabled = 0
+options = st.multiselect(
+        'Select Planning area',
+        planning_areas,
+        planning_areas[1],
+        max_selections=1,
+        disabled=st.session_state.disabled)
 
+try:
+    if(st.session_state.disabled == 0):
+        st.write('You selected:', options[0])
+    area_selected = options[0]
+    # index = data[data['name']==name_selected].index[0]
+    index = planning_areas.index(area_selected)
+    planningAreaFilter = planning_areas[index]
+except:
+    st.write("You haven't chosen an option")
+    name_selected = ''
+    index = None
 
-
-values = st.sidebar.slider(
-     'Select frequency range:',
-     floor(min(period)), ceil(np.percentile(period, 95)), (floor(min(period)), ceil(np.percentile(period, 95))))
-
-
-inperiod = [True if (i >= values[0] and i <= values[1]) else False for i in period]
-color_dict = {True:'#17becf', False:'#D3D3D3'}
-
-base=folium.Map([1.36, 103.83],zoom_start=11.5,tiles="cartodbpositron")
-for i in range(len(pos)):
-    # for pos_lat_long in bus_stops:
-    #     # print("printing:",pos_lat_long)
-    folium.PolyLine(
-            pos[i], # tuple of coordinates 
-            color = color_dict[inperiod[i]], # map each segment with the speed 
-            # colormap = color_dict, # map each value with a color 
-            ).add_to(base)
-        # print(pos_lat_long)
-st_folium(base, width=1200, height=600)
+if(index is not None):
+    filter_links = filter_by_planning_area(data,planningAreaFilter,bus_stops)
+    base=folium.Map([1.36, 103.83],zoom_start=11.5,tiles="cartodbpositron")
+    for i in range(len(filter_links)):
+        # for pos_lat_long in bus_stops:
+        #     # print("printing:",pos_lat_long)
+        folium.PolyLine(
+                filter_links[i], # tuple of coordinates 
+                # color = color_dict[inperiod[i]], # map each segment with the speed 
+                # colormap = color_dict, # map each value with a color 
+                ).add_to(base)
+            # print(pos_lat_long)
+    st_folium(base, width=1200, height=600)
